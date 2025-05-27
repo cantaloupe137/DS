685fe0d3a8c969aa0662386e89c47f3759bbd06d
WDAGUtilityAccount <cantaloupe137@gmail.com>
2025-05-27 13:21:55 +0800

    update PA

--- w13-queue/PA.cpp
+++ w13-queue/PA.cpp
@@ -1,34 +1,48 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
 
-int main() {
+int main()
+{
     int N;
-    while (cin >> N) {
-        if (N == 0) {
+    while (cin >> N)
+    {
+        if (N == 0)
+        {
             cout << 0 << endl;
             continue;
         }
         queue<int> q;
-        for (int i = 0; i < N; ++i) {
+        for (int i = 0; i < N; ++i)
+        {
             int x;
             cin >> x;
             q.push(x);
         }
-        while (q.size() > 1) {
+        while (q.size() > 1)
+        {
             vector<int> v;
-            while (!q.empty()) {
+            while (!q.empty())
+            {
                 v.push_back(q.front());
                 q.pop();
             }
             sort(v.begin(), v.end());
-            int y = v.back(); v.pop_back();
-            int x = v.back(); v.pop_back();
-            if (y != x) v.push_back(y - x);
+            int y = v.back();
+            v.pop_back();
+            int x = v.back();
+            v.pop_back();
+            if (y != x)
+                v.push_back(y - x);
 
-            for (int stone : v) q.push(stone);
+            for (int stone : v)
+                q.push(stone);
         }
-        if (q.empty()) cout << 0 << endl;
-        else cout << q.front() << endl;
+        if (q.empty())
+            cout << 0 << endl;
+        else
+            cout << q.front() << endl;
     }
     return 0;
 }
